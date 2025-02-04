3f4bcb9d48c4fb2925ccbdd0d6acfe54c12855c2
Mohammad Sahil Khan <134322190+Mdsahil0918@users.noreply.github.com>
2025-02-04 21:35:38 +0000

    datatype in js

---
+++ 0_1basics/test.js
@@ -0,0 +1,12 @@
+const accountId = 7233
+let accountEmail = "sahil@gmail.com"
+var accountPassword = "12345"
+accountCity= "Hyd"
+let accountState
+
+
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountState])
+// Use let if reassignment is needed.
+// Use const by default (for values that shouldn’t change).
+// Avoid var (legacy, can lead to bugs due to function-scoping and hoisting).
