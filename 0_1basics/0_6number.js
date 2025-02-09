1c0f34abeb894273d99483cb4fa30e649cd1574b
Mohammad Sahil Khan <134322190+Mdsahil0918@users.noreply.github.com>
2025-02-09 16:21:53 +0000

    math and its funtions

---
+++ 0_1basics/0_6number.js
@@ -0,0 +1,23 @@
+// const score = 400
+// console.log(score)
+// const balance=new Number(100)
+// console.log(balance)
+// console.log(balance.toString().split())
+// // console.log(score.toFixed(3))  400.000
+// const other=23.9292
+// console.log(other.toPrecision(3))
+// const hund=100000
+// console.log(hund.toLocaleString('en-IN'))
+// +++++++++++ Math ++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-22));
+// console.log(Math.round(4.5490));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8))
+// console.log(Math.pow(3,3));
+console.log(Math.random());
+console.log(Math.round(Math.random()*10)+1);
+
+const min=25
+const max =100
+console.log(Math.floor(Math.random()* (max-min+1))+min)
