92150748993dac02756a795f3f7fcc7f46593a06
Mohammad Sahil Khan <134322190+Mdsahil0918@users.noreply.github.com>
2025-02-09 05:30:37 +0000

    op and conversions

---
+++ 0_1basics/convertion_operation.js
@@ -0,0 +1,32 @@
+// let score=null o/p=0
+// let score="33" o/p=33 type is number
+// let score="33ac" 0/p Nan and the type is number
+// true=1 & false =0 
+
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn=1
+// let isBool = Boolean(isLoggedIn)
+// console.log(isBool)
+// console.log(typeof isBool)
+// let num=99
+// let strnum = String(num)
+// console.log(strnum)
+// console.log(typeof strnum)
+// +++++++++++++++++++Operations+++++++++++++++++++++++++
+let value=9
+let negValue=-value
+// console.log(negValue)
+
+console.log([] == false);
+// console.log([] === false);
+console.log(null == undefined);
+console.log(null === undefined);
+let x = 3;
+console.log(x++);  
+console.log(++x);  
+console.log(x--);  
+console.log(--x);
